1b991bf53685a7159b39ca4981c34cde0b548bb2
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-03-14 12:10:50 +0100

    feat(alf): Add script to run alf interpreter
    It supports arbitrary (relative) Model roots

---
+++ alf.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+REPO_DIR="$(dirname "$(realpath "$0")")"
+
+ALF_BUILD_DIR="$REPO_DIR/build/alf"
+
+CWD="$(pwd)"
+
+if ! [ -e "$ALF_BUILD_DIR/dist/alf.jar" ]; then
+  >&2 echo "Alf jar not found. Did you run the installAlf gradle task"
+  exit 1
+fi
+
+cd "$ALF_BUILD_DIR"
+
+args=( "${@}" )
+#args+=( "__end__" )
+finalArgs=""
+
+modelDirWasFixed=false
+nextIsModelDir=false
+
+for var in "${args[@]}" ;do
+  if $nextIsModelDir ;then
+    nextIsModelDir=false
+    modelDirWasFixed=true
+    modelDir="$var"
+    if ! [[ "$modelDir" == "/"* ]] ;then
+      modelDir="$CWD/$modelDir"
+      >&2 echo "Replacing model directory with $modelDir"
+    fi
+    finalArgs="$finalArgs $modelDir"
+  else
+    if [ "$var" == "-m" ] ;then
+      nextIsModelDir=true
+    fi
+    finalArgs="$finalArgs $var"
+  fi
+done
+
+if ! $modelDirWasFixed ;then
+  modelDir="$CWD/Models"
+  >&2 echo "Setting model dir to $modelDir"
+  finalArgs="-m $modelDir $finalArgs"
+fi
+
+java -jar "dist/alf.jar" $finalArgs
